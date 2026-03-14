e467c2105d732445d617f308f69bbc80812da1ca
esraaehab333 <esraaehabb333@gmail.com>
2026-03-14 10:34:37 +0200

    feat: handle empty data state in FavoriteViewModel and display EmptyStateScreen

--- app/src/main/java/com/example/naseem/presentation/fav/view/FavoriteScreen.kt
+++ app/src/main/java/com/example/naseem/presentation/fav/view/FavoriteScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.naseem.R
@@ -82,19 +83,34 @@ fun FavoriteScreen(
                 }
                 is ApiState.Success -> {
                     val places = (state as ApiState.Success).data
-                    LazyColumn(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.spacedBy(12.dp),
-                        contentPadding = PaddingValues(bottom = 80.dp)
-                    ) {
-                        items(places) { place ->
-                            WeatherCard(
-                                place = place,
-                                color = color,
-                                onDeleteClick = {
-                                    viewModel.deleteFromFavorites(place)
-                                }
+                    if (places.isEmpty()) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            EmptyStateScreen(
+                                color=color,
+                                icon = R.drawable.ic_favorite_map,
+                                title ="No saved places yet",
+                                subtitle = "Add your favorite cities to stay updated on their weather conditions."
                             )
+                        }
+                    }
+                    else{
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.spacedBy(12.dp),
+                            contentPadding = PaddingValues(bottom = 80.dp)
+                        ) {
+                            items(places) { place ->
+                                WeatherCard(
+                                    place = place,
+                                    color = color,
+                                    onDeleteClick = {
+                                        viewModel.deleteFromFavorites(place)
+                                    }
+                                )
+                            }
                         }
                     }
                 }
